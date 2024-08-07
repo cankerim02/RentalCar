f9180ab07ff16fb03fb1ffff963b0fd5f0c56896
cankerim02 <cankerim02@gmail.com>
2024-08-07 20:36:47 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public string CarName { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Descriptions { get; set; }
+    }
+}
